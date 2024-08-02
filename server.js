1f2180c2baa1059bcc682aaf9e8e5f7a8653f5a7
Yash <kudveyash@gmail.com>
2024-08-02 17:27:33 +0530

    changes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=>{
+    // console.log('Hello');
+    // res.send("hey there");
+
+    // res.sendStatus(500)
+    // res.status(500).send("Some error occured")
+    // res.status(500).json({message: 'Error occured', key:'123'})
+
+    res.render('index')
+
+})
+
+app.listen(3000)
