bbee42ca75d03252eddbbcd11513bd126cd0b721
Yash <kudveyash@gmail.com>
2024-08-08 17:31:46 +0530

    validation schemas

---
+++ utils/validationSchemas.js
@@ -0,0 +1,17 @@
+export const createUserValidationSchema = {
+    username:{
+        isLength:{
+            options:{
+                min: 5,
+                max:32
+            },
+            errorMessage:"Username must be between 3-32 characters"
+        },
+        notEmpty:{
+            errorMessage:"Username cannot be empty"
+        },
+        isString:{
+            errorMessage:"Username must be string"
+        },
+    },
+}
