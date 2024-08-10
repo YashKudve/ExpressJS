42ea0528ad605b217d82c6745a65acd2ab2cb593
Yash <kudveyash@gmail.com>
2024-08-10 16:47:01 +0530

    cookies;cookie parser

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import routes from './routes/index.mjs'
+import cookieParser from "cookie-parser";
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use(cookieParser())
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,7 +14,7 @@ app.listen(PORT,()=>(
     console.log(`Running at port ${PORT}`)
 ))
 
-app.get('/',(req,res)=>{
-    res.cookie("hello", "world", {maxAge: 60000})
+app.get("/",(req,res)=>{
+    res.cookie("hello", "world", {maxAge: 60000 * 60 * 2})
     res.status(201).send({msg:"Hello"})
 })
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req,res)=>{
-    res.send([{id:111, name:"Chicken", price: 230}])
+    console.log(req.headers.cookie)
+    console.log(req.cookies);
+
+    if(req.cookies.hello && req.cookies.hello === "world")
+       return res.send([{id:111, name:"Chicken", price: 230}]);
+    
+    else
+    return res.send({msg: "Sorry. You need the correct cookie"});
+       
 })
 
 export default router;
