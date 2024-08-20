4e9a02d68972e470467ce01f50267ac82d3dce75
Yash <kudveyash@gmail.com>
2024-08-20 15:47:05 +0530

    authentication from mongodb database

--- src/strategies/localStrategy.mjs
+++ src/strategies/localStrategy.mjs
@@ -1,6 +1,7 @@
 import passport from 'passport'
 import { Strategy } from 'passport-local'
 import { mockUsers } from '../utils/constants.mjs'
+import { User } from '../mongoose/schema/user.mjs'
 
 passport.serializeUser((user,done)=>{
     console.log("Inside Serializer")
@@ -9,13 +10,13 @@ passport.serializeUser((user,done)=>{
     done(null,user.id)
 })
 
-passport.deserializeUser((id,done)=>{
+passport.deserializeUser(async(id,done)=>{
     console.log(`Inside Deserializer`)
     console.log(`Deserizlizer User ID: ${id}`);
-    
-
+   
     try {
-        const findUser = mockUsers.find((user)=>user.id === id)
+        // const findUser = mockUsers.find((user)=>user.id === id)
+        const findUser = await User.findById(id)
         if(!findUser) throw new Error("User not found");
         done(null,findUser);
 
@@ -24,19 +25,22 @@ passport.deserializeUser((id,done)=>{
     }
 })
 export default passport.use(
-    new Strategy((username,password,done)=>{
+    new Strategy(async(username,password,done)=>{
         console.log(`Username: ${username}`)
         console.log(`Password: ${password}`)
         try {
-            const findUser = mockUsers.find((user)=> user.username === username)
-    
-            if(!findUser) throw new Error('User not found');
-            
-            if(findUser.password !== password) throw new Error('Invalid Credentials');
+            // const findUser = mockUsers.find((user)=> user.username === username)
+            // if(!findUser) throw new Error('User not found');
+            // if(findUser.password !== password) throw new Error('Invalid Credentials');
+            // done(null,findUser)
 
+            const findUser = await User.findOne({username});
+            if(!findUser) throw new Error("User not found");
+
+            if(findUser.password !== password) throw new Error("Bad Credentials");
             done(null,findUser)
         } catch (error) {
-            done(err,null)
+            done(error,null)
         }
     })
 )
