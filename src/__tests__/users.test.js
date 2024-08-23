e6ce14009f5a67f30caff9199ee43ae30290f1e7
Yash <kudveyash@gmail.com>
2024-08-23 18:15:15 +0530

    test file created

---
+++ src/__tests__/users.test.js
@@ -0,0 +1,11 @@
+import { getUserByIdHandler } from "../handlers/users.mjs";
+
+const mockRequest = {};
+
+const mockResponse = {};
+
+describe('get users', ()=>{
+    it('should get user by id', ()=>{
+        getUserByIdHandler()
+    })
+})
