03933339e52b2f66a7e387ffadb960d70f6b37d0
Yash <kudveyash@gmail.com>
2024-08-29 16:46:19 +0530

    test case

--- src/__tests__/users.test.js
+++ src/__tests__/users.test.js
@@ -14,11 +14,17 @@ describe('get users', ()=>{
         getUserByIdHandler(mockRequest, mockResponse);
         expect(mockResponse.send).toHaveBeenCalled();
         expect(mockResponse.send).toHaveBeenCalledWith({
-            id: 2,
-            username:'chandan',
-            displayName: 'CHANDAN',
-            password:"hello124"
+            id: 1,
+            username:'yash',
+            displayName: 'YASH',
+            password:"hello123"
         });
         expect(mockResponse.send).toHaveBeenCalledTimes(1);
+    })
+
+    it("should call status 404 if user not found", ()=>{
+        const copyMockRequest = { ...mockRequest, findUserIndex:100};
+        getUserByIdHandler(copyMockRequest,mockResponse);
+        expect(mockResponse.sendStatus).toHaveBeenCalled();
     })
 })
